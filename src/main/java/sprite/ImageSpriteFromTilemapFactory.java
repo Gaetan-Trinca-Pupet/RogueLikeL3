a84191c62f2862cfbc72f6c7e7619ec62654af01
raphael lesbros <raphael.lesbros@gmail.com>
2021-11-11 16:34:55 +0100

    Ajout d'une factory d'image depuis un tileset

---
+++ src/main/java/sprite/ImageSpriteFromTilemapFactory.java
@@ -0,0 +1,15 @@
+package sprite;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritableImage;
+import utilities.Vector2D;
+
+public class ImageSpriteFromTilemapFactory {
+    public static ImageSprite construct(String path, int col, int row, Vector2D position, Vector2D size) {
+        System.out.println("src/resources/" + path);
+        PixelReader reader = new Image("file:src/resources/" + path).getPixelReader();
+        Image croppedImage = new WritableImage(reader, col*17, row*17, 16, 16);
+        return new ImageSprite(croppedImage, position, size);
+    }
+}
