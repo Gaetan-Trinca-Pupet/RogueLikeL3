57094a212b8da605f0b81e460a934fec372f32dc
Solal <bompais.solal@gmail.com>
2021-11-22 18:21:06 +0100

    Interface combat brrr. Des bisous. :*

--- src/main/java/gameComponents/FightState.java
+++ src/main/java/gameComponents/FightState.java
@@ -1,6 +1,9 @@
 package gameComponents;
 
 import Controller.Action;
+import EventManager.MouseEventManager;
+import collision.Collision;
+import collision.SquareCollision;
 import entity.Creature;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
@@ -27,13 +30,63 @@ public class FightState extends GameState{
         Sprite border = new Square(pos.subtract(new Vector2D(10,10)), size.add(new Vector2D(20,20)), new Color(1,1,1,1));
         Sprite centerSquare = new Square(pos, size, new Color(0,0,0,1));
 
-        spriteList = new SpriteHandler();
-        spriteList.addHandlerToGround(Ground.BACKGROUND, lastState.getSpriteList());
-
-        Vector2D screenSize = gameContext.gameWindow.getScreenSize();
-        spriteList.addSpriteTo(Ground.GROUND, new Square(screenSize.divideBy(new Vector2D(-2,-2)),screenSize, new Color(0,0,0,0.5)));
         spriteList.addSpriteTo(Ground.GROUND, border);
         spriteList.addSpriteTo(Ground.GROUND, centerSquare);
+
+        // Buttons
+        Vector2D sizeButton = new Vector2D(200, 100);
+        Sprite label;
+
+        // Attack button
+        Vector2D posAttackButton = new Vector2D(sizeButton.x * -2, 0);
+        attackCollision = new SquareCollision(new Vector2D(posAttackButton), sizeButton);
+        CompositeSprite attackButton = new CompositeSprite(posAttackButton);
+
+        border = new Square(new Vector2D(-10, -10), sizeButton.add(new Vector2D(20,20)), new Color(1,1,1,1));
+        centerSquare = new Square(new Vector2D(), sizeButton, new Color(0,0,0,1));
+        label = new LabelSprite("Attaquer", new Color(1,1,1,1), new Vector2D(10,10));
+
+
+        attackButton.add(border);
+        attackButton.add(centerSquare);
+        attackButton.add(label);
+
+        // Inventory button
+        Vector2D posInventoryButton = new Vector2D(sizeButton.x, 0);
+        inventoryCollision = new SquareCollision(new Vector2D(posAttackButton), sizeButton);
+        CompositeSprite inventoryButton = new CompositeSprite(posInventoryButton);
+
+        border = new Square(new Vector2D(-10, -10), sizeButton.add(new Vector2D(20,20)), new Color(1,1,1,1));
+        centerSquare = new Square(new Vector2D(), sizeButton, new Color(0,0,0,1));
+        label = new LabelSprite("Inventaire", new Color(1,1,1,1), new Vector2D(10,10));
+
+        inventoryButton.add(border);
+        inventoryButton.add(centerSquare);
+        inventoryButton.add(label);
+
+        // Buttons
+        CompositeSprite buttons = new CompositeSprite(size.multiply(new Vector2D(0, 0.25)));
+        attackCollision.getPosition().add(buttons.getPosition());
+        inventoryCollision.getPosition().add(buttons.getPosition());
+        buttons.add(attackButton);
+        buttons.add(inventoryButton);
+
+        spriteList.addSpriteTo(Ground.FOREGROUND, buttons);
+
+
+
+        // Player
+        CompositeSprite playerSprite = new CompositeSprite(player.getSprite().getPosition().multiply(new Vector2D(-1,-1)));
+        playerSprite.add(player.getSprite());
+        playerSprite.setPosition(playerSprite.getPosition().add(new Vector2D()));
+        spriteList.addSpriteTo(Ground.FOREGROUND, playerSprite);
+
+        // Monster
+        Sprite monsterSprite = monster.getSprite();
+        monsterSprite.setPosition(new Vector2D(size).divideBy(new Vector2D(-8,-4)));
+        spriteList.addSpriteTo(Ground.FOREGROUND, monsterSprite);
+
+        spriteList.addHandlerToGround(Ground.BACKGROUND, lastState.getSpriteList());
     }
 
     @Override
@@ -49,7 +102,12 @@ public class FightState extends GameState{
 
     @Override
     public void mouseEvent(MouseEvent event) {
-
+        if(event.getEventType() == MouseEvent.MOUSE_PRESSED)
+            player.getInventory().eventController(controller);
+        for(MouseEventManager mouseEvent : mouseEventList)
+            mouseEvent.mouseEvent(event);
+        System.out.println(attackCollision.isInside(controller.getMousePosition()));
+        System.out.println(inventoryCollision.isInside(controller.getMousePosition()));
     }
 
     @Override
